cffdf55140bd3a4f34e523a21fc516d0fc062128
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-02-05 10:07:07 +0530

    footer desighn completed

--- application/views/common_components/footer.php
+++ application/views/common_components/footer.php
@@ -1,71 +1,76 @@
-
-
 <link href="<?php echo base_url('assets/css/bootstrap.min1.css')?>" rel="stylesheet" />
 <link href="<?php echo base_url('assets/css/custom.css')?>" rel="stylesheet" />
 <link href="<?php echo base_url('assets/css/footer.css')?>" rel="stylesheet" />
 
 <footer class="limits-footer-wrapper">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 text-center order-md-0 order-1">
-                    <h1>Book Now</h1>
-                    <h2>Tel: <a href="tel:02039822911" class="footer-contact">02039822911</a></h2>
-                </div>
-                <div class="col-md-4 no-gutter text-center order-md-1 order-0">
-                    <img src="<?php echo base_url('assets/images/logo.png')?>" class="img-fluid footr-logo" alt="Logo">
-                    
-                </div>
-                <div class="col-md-4 text-center order-md-2 order-2">
-                    <h1>Email us</h1>
-                    <h2><a href="mailto:info@nolimitcars.co.uk" class="footer-contact">info@nolimitcars.co.uk</a></h2>
-                </div>
-            </div>
+    <div class="footer-container">
+        <div class="mx-auto no-gutter text-center order-md-1 order-0">
+            <img src="<?php echo base_url('assets/images/travel24/Logo.svg')?>" class="img-fluid footr-logo" alt="Logo">
+
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <h3>All major credit cards accepted</h3>
+                    <h3 class="footer-h3">All major credit cards accepted</h3>
                     <ul class="social-media">
                         <li>
-                            <a href="https://twitter.com/nolimit_cars" target="_blank"><img src="<?php echo base_url('assets/images/twitter.png')?>" alt="icon"></a>
+                            <a href="https://twitter.com/nolimit_cars" target="_blank"><img
+                                    src="<?php echo base_url('assets/images/travel24/twitter.svg')?>" alt="icon"></a>
                         </li>
                         <li>
-                            <a href="https://www.linkedin.com/company/nolimitcars/" target="_blank"><img src="<?php echo base_url('assets/images/linkedin.png')?>" alt="icon"></a>
+                            <a href="https://www.instagram.com/nolimitcars8/" target="_blank"><img
+                                    src="<?php echo base_url('assets/images/travel24/insta.svg')?>" alt="icon"></a>
                         </li>
                         <li>
-                            <a href="https://www.instagram.com/nolimitcars8/" target="_blank"><img src="<?php echo base_url('assets/images/insta.png')?>" alt="icon"></a>
-                        </li>
-                        <li>
-                            <a href="https://www.facebook.com/nolimitcarsltd/" target="_blank"><img src="<?php echo base_url('assets/images/facebook.png')?>" alt="icon"></a>
+                            <a href="https://www.facebook.com/nolimitcarsltd/" target="_blank"><img
+                                    src="<?php echo base_url('assets/images/travel24/facebook.svg')?>" alt="icon"></a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="row mt-md-4 mt-3">
-                <div class="col-md-6">
-                    <ul class="side-menu">
-                        <li><a href="">© NOLIMIT Cars 2017</a></li>
-                        <!--<li><a href="https://www.nolimitcars.co.uk/wp/about-us/">About</a></li>-->
-                        <li><a  href="<?php echo base_url()?>terms">Terms</a></li>
-                        <li><a  href="<?php echo base_url()?>drivers">Drivers</a></li>
-                        <li><a  href="<?php echo base_url()?>aboutus">About Us</a></li>
-                        <li><a  href="<?php echo base_url()?>contactUs">contact</a></li>
-                        
-                        <!--<li><a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Areas</a>-->
-                    </ul>
-                </div>
-                <div class="col-md-6">
-                   <h6 class="credit"><a href="https://www.solutions2xl.com/" target="_blank" ></a></h6>
-                </div>
-            </div>
-            
         </div>
-    </footer>
+
+        <div class="row mt-md-4 mt-3">
+            <div class="col-md-6">
+                <ul class="side-menu ">
+                    <li><a href="<?php echo base_url()?>terms" class="footer-p1">Terms</a></li>
+                    <li><a href="<?php echo base_url()?>drivers" class="footer-p1">Drivers</a></li>
+                    <li><a href="<?php echo base_url()?>aboutus" class="footer-p1">About Us</a></li>
+                    <li><a href="<?php echo base_url()?>contactUs" class="footer-p1">contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6 ">
+                <div class="text-right">
+                    <ul class="side-menu  ">
+                        <li><a href="<?php echo base_url()?>terms" class="footer-p1">Tel: 02039822911 </a></li>
+                        <li><a href="<?php echo base_url()?>drivers" class="footer-p1">Email Us :
+                                info@nolimitcars.co.uk</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-md-4 mt-3">
+            <div class="col-md-6">
+                <div class=" text-center text-md-left">
+                    <span class="footer-p2 mt-3">
+                        © NOLIMIT Cars 2017
+                    </span>
+                </div>
+            </div>
+            <div class="col-md-6 ">
+                <div class="text-center text-md-right">
+                    <span class="footer-p2 mt-3">
+                        Terms & Conditions
+                    </span>
+                </div>
+            </div>
+        </div>
+</footer>
 
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url('assets/js/jquery.3.0.min.js')?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.touchSwipe.min.js')?>"></script>
-    <script src="https://use.fontawesome.com/1e36072efd.js"></script>
-    <script src="<?php echo base_url('assets/js/custom.js')?>"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url('assets/js/jquery.3.0.min.js')?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.touchSwipe.min.js')?>"></script>
+<script src="https://use.fontawesome.com/1e36072efd.js"></script>
+<script src="<?php echo base_url('assets/js/custom.js')?>"></script>
