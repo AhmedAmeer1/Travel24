8c7924034c8dafad785b255f67339f4a2f0a57f4
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-02-08 07:03:52 +0530

    terms and condition page done

--- application/views/terms.php
+++ application/views/terms.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,42 +11,157 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-230246454-1"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
     gtag('js', new Date());
 
     gtag('config', 'UA-230246454-1');
-    </script> 
+    </script>
 </head>
+
 <body>
-    
-<?php $this->load->view('common_components/header'); ?>
-    <section id="content">
-        <section class="commonbanner">
-            <div class="sub_banner_overlay"></div>
-            <img src="<?php echo base_url('assets/images/nolimit-banner.jpg')?>">
-        </section>
-        <section class="subpagecontent">
-            <img src="<?php echo base_url('assets/images/nolimit-body.jpg')?>">
-            <div class="wrapper">
-                <div class="subcontent_wrapper">
-                    <h1>Terms and Conditions</h1>
-                    <p>You will receive a welcome email containing your login details and password on your first booking with us. you will automatically receive a NoLimit Cars acknowledgement email for the journey you have made. We will send your journey details automatically. You have to check your email and all details are correct which provided. If there is any change call us or email us. If you missed to do so NoLimit Cars won’t responsible for your journey.</p>
-                    <p>We will send your journey details, pick up instructions and driver phone number when the journey has been assigned to a driver. You have to check your email and phone time to time your journey begins. Driver will call or you can call driver. If you missed to do so NoLimit Cars will not responsible and will not refundable. You have to follow all the instructions.</p>
-                    <p>We advise passenger to arrive 3 hours early as possible to the airport. Other places are depend on the check in time. Unpredicted traffic delays and routes NoLimit Cars won’t responsible for your missed flight or other appointments.</p>
-                    <p>NoLimit Cars will not accept any responsibilities for missed flight or any other appointments. What ever reason eg; severe weather, traffic delays and accidents unforseen circumstances.</p>
-                    <p>Unfortunately for vehicle breakdown we will arrange for another vehicle to get your destination. Free of charge (but depends on the circumstances) means how long time will take. If missed the flight or other appointments we won’t take any responsibilities.</p>
-                    <p>Please make sure you provide correctly date and time of departure or arrival in UK time zone.</p>
-                    <p>Free of changeable date and time if you change the vehicle. Different price for what size vehicle you want. You have to tell us one hour before pick up time other wise 25% chargeable.</p>
-                    <p>Free of charge for cancelling before one hour time of journey if you missed to do so it will be 25% cancellation fee.</p>
-                    <p>NoLimit Cars will not take any responsible for passengers take other alternative transport. (no refundable for the cost) you have to wait for the driver or call the driver or the NoLimit Cars.</p>
-                    <p>If there is any change in journey times you must inform NoLimit Cars as soon as possible if you missed to do so no refundable.</p>
-                    <p>NoLimit Cars advise you to book the right size vehicle for passengers and luggages. If exceeding of luggages and passengers we wont take any responsibilities for that. if you are not sure about the capacity of vehicle you booked with us please give us call 24/7 to NoLimit Cars.</p>
-                    <p>NoLimit Cars use their own transport most of the time. Sometimes NoLimit Cars do use third party company appropriate due to the volume of bookings we have.</p>
+
+    <?php $this->load->view('common_components/header'); ?>
+    <section>
+
+        <div class="container-fluid banner-section">
+            <img src="<?php echo base_url('assets/images/travel24/terms_conditions.svg')?>" alt="about_us_banner"
+                class="banner-image">
+        </div>
+        <div class=" aboutus_wrapper">
+            <div class="aboutus_content_div">
+                <h1>Terms & Conditions</h1>
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        You will receive a welcome email containing your login details and password on your first
+                        booking with us. you will automatically receive a NoLimit Cars acknowledgement email for the
+                        journey you have made. We will send your journey details automatically. You have to check your
+                        email and all details are correct which provided. If there is any change call us or email us. If
+                        you missed to do so NoLimit Cars won’t responsible for your journey.
+                    </p>
                 </div>
+
+
+
+                <div class="d-flex">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        We will send your journey details, pick up instructions and driver phone number when the journey
+                        has been assigned to a driver. You have to check your email and phone time to time your journey
+                        begins. Driver will call or you can call driver. If you missed to do so NoLimit Cars will not
+                        responsible and will not refundable. You have to follow all the instructions.
+                    </p>
+                </div>
+
+
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        We advise passenger to arrive 3 hours early as possible to the airport. Other places are depend
+                        on the check in time. Unpredicted traffic delays and routes NoLimit Cars won’t responsible for
+                        your missed flight or other appointments.
+                    </p>
+                </div>
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        NoLimit Cars will not accept any responsibilities for missed flight or any other appointments.
+                        What ever reason eg; severe weather, traffic delays and accidents unforseen circumstances.
+                    </p>
+                </div>
+
+
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        Unfortunately for vehicle breakdown we will arrange for another vehicle to get your destination.
+                        Free of charge (but depends on the circumstances) means how long time will take. If missed the
+                        flight or other appointments we won’t take any responsibilities.
+                    </p>
+                </div>
+
+
+
+
+                <div class="d-flex">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        Please make sure you provide correctly date and time of departure or arrival in UK time zone.
+                    </p>
+                </div>
+
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        Free of changeable date and time if you change the vehicle. Different price for what size
+                        vehicle you want. You have to tell us one hour before pick up time other wise 25% chargeable.
+                    </p>
+                </div>
+
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        NoLimit Cars will not take any responsible for passengers take other alternative transport. (no
+                        refundable for the cost) you have to wait for the driver or call the driver or the NoLimit Cars.
+                    </p>
+                </div>
+
+                <div class="d-flex ">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        If there is any change in journey times you must inform NoLimit Cars as soon as possible if you
+                        missed to do so no refundable.
+                    </p>
+                </div>
+
+
+                <div class="d-flex">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        NoLimit Cars advise you to book the right size vehicle for passengers and luggages. If exceeding
+                        of luggages and passengers we wont take any responsibilities for that. if you are not sure about
+                        the capacity of vehicle you booked with us please give us call 24/7 to NoLimit Cars.
+                    </p>
+                </div>
+
+                <div class="d-flex">
+                    <img src="<?php echo base_url('assets/images/travel24/terms.svg')?>" alt="terms_icon"
+                        class="terms_icon">
+                    <p>
+                        NoLimit Cars use their own transport most of the time. Sometimes NoLimit Cars do use third party
+                        company appropriate due to the volume of bookings we have.
+                    </p>
+                </div>
+
+
+
+                <div class="subbottom_text">If you would like a quote for a journey you are planning please use our
+                    online booking form or call us on&nbsp;<a href="tel:02039822911">02039 822 911</a>.</div>
             </div>
-        </section>
+
+        </div>
     </section>
     <?php $this->load->view('common_components/footer'); ?>
     <script src="<?php echo base_url('assets/js/custom.js')?>"></script>
 </body>
+
 </html>
