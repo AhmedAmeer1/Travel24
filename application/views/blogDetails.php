46b812f3238664560eb40bb7f283a3691a19ad3b
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-02-11 19:36:31 +0530

    details blog page added

--- application/views/blogDetails.php
+++ application/views/blogDetails.php
@@ -10,7 +10,7 @@
 
     <title>Blogs</title>
     <link rel="icon" type="img/png" sizes="32x32" href="<?php echo base_url('assets/images/favicon.png')?>">
-    <link rel="stylesheet" href="<?= base_url('assets/css/blogs.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/blogDetails.css') ?>">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-230246454-1"></script>
@@ -47,9 +47,52 @@
 
     <section class="carlist-wrapper">
         <div class="home_container no-gutter-responsive">
+            <div class="blog_heading">
+                <h1>Balancing work and life as a PCO driver </h1>
+                <p>28 December 2023</p>
+            </div>
 
-            <h1>Our latest blogs </h1>
-            <p>Uncover the vibrant stories that unfold on the streets in our "Ride Chronicles" blog series.</p>
+            <div class="blog_details mt-5">
+                <h1>Perks </h1>
+                <p>
+                    With many large ride-hailing companies failing to provide their drivers with everything they need to
+                    feel safe, secure, and satisfied in their profession, many drivers have reasonably come to expect
+                    very little in the way of job perks.
+                    However, you can expect a lot more as an Addison Lee Driver.
+                </p>
+                <p>
+                    Here are some of the benefits of driving with us:
+                </p>
+
+
+                <h1>Pay </h1>
+                <p>
+                    At 24 hours taxi, we offer our customers a range of different services, meaning these differences
+                    carry through to you, the driver.
+                </p>
+                <p>
+                    While most ride-hailing and taxi companies give their drivers little or no choice regarding the type
+                    of service they provide customers, with Addison Lee, the decision lands with you. That means you can
+                    expect more freedom and more ways to find a service that you’re
+                    passionate about.
+                </p>
+
+
+
+                <h1>Wait Times </h1>
+                <p>
+                    Many people expect a lot of drivers’ time will be spent waiting or searching for jobs. Addison Lee
+                    is different. We are the UK’s largest private hire, Black Taxi, and same-day delivery company,
+                    completing over 4,500 jobs daily in London alone.
+                </p>
+
+            </div>
+
+
+            <div class="related_posts">
+                <h1>Related Posts</h1>
+              
+            </div>
 
             <div class="row mt-5 no-gutter-responsive">
 
@@ -65,14 +108,9 @@
                                 sure …
                             </p>
                             <div class="d-flex justify-start ">
-                                <a href="<?php echo base_url()?>blogDetails" class="">
-                                    <h3>Read post</h3>
-                                </a>
-                                <a href="<?php echo base_url()?>blogDetails" class="">
-                                    <img src="assets/images/travel24/blogs/arrow.svg" class="img-fluid mt-4 ml-4 "
-                                        alt="car">
-                                </a>
-
+                                <h3>Read post</h3>
+                                <img src="assets/images/travel24/blogs/arrow.svg" class="img-fluid mt-3 ml-4 "
+                                    alt="car">
                             </div>
                         </div>
                     </div>
@@ -98,47 +136,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="car-box">
-                        <div class="w-100 blogs_image_card">
-                            <img src="assets/images/travel24/blogs/blog_5.svg" class="img-fluid w-100" alt="car">
-                        </div>
-                        <div class="text_card">
-                            <h2>5 ways to maximise your income as a PCO driver</h2>
-                            <p>
-                                Working as a private hire driver in London can be a lucrative job – but how do you make
-                                sure …
-                            </p>
-                            <div class="d-flex justify-start ">
-                                <h3>Read post</h3>
-                                <img src="assets/images/travel24/blogs/arrow.svg" class="img-fluid mt-3 ml-4 "
-                                    alt="car">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-
-                <div class="col-md-6">
-                    <div class="car-box">
-                        <div class="w-100 blogs_image_card">
-                            <img src="assets/images/travel24/blogs/blog_6.svg" class="img-fluid w-100" alt="car">
-                        </div>
-                        <div class="text_card">
-                            <h2>Overview of london traffic laws for PCO drivers</h2>
-                            <p>
-                                Working as a private hire driver in London can be a lucrative job – but how do you make
-                                sure …
-                            </p>
-                            <div class="d-flex justify-start ">
-                                <h3>Read post</h3>
-                                <img src="assets/images/travel24/blogs/arrow.svg" class="img-fluid mt-3 ml-4 "
-                                    alt="car">
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              
 
 
 
