7b6764c886d92a2758a1095dfe263204bc65f221
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-03-06 21:21:14 +0530

    letter head test

--- application/views/template/email.php
+++ application/views/template/email.php
@@ -16,16 +16,13 @@
         </div>
         <div style="padding: 20px; ">
             <div>
-
-                <div class="row ">
+            <div class="row ">
                     <div class="col-md-6">
                         <a class="mt-2" href="<?php echo base_url()?>"><img
-                                src="<?php echo base_url('assets/images/travel24/Logo.svg')?>" alt=""></a>
+                                src="https://travel24taxi.com/assets/images/travel24/Logo.svg" alt=""></a>
                     </div>
                     <div class="col-md-6 text-right">
-                        For any inquiries, contact <a href="tel:02039822911 " class=" ">
-                            <img src="<?php echo base_url('assets/images/travel24/call.svg')?>" alt="">
-                            02039822911</a>
+                        For any inquiries, contact <a href="tel:02039822911 " class=" "> 02039822911</a>
                     </div>
                 </div>
 
@@ -337,7 +334,13 @@
 
             </div><?php */?>
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('assets/js/jquery.3.0.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/jquery.touchSwipe.min.js')?>"></script>
+    <script src="https://use.fontawesome.com/1e36072efd.js"></script>
+    <script src="<?php echo base_url('assets/js/custom.js')?>"></script>
         </div>
     </div>
 </body>
