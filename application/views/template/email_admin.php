bebc1dda4dd60143ae9aab7afee2907aadfb0dc6
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-04-23 11:29:16 +0530

    testing

--- application/views/template/email_admin.php
+++ application/views/template/email_admin.php
@@ -26,6 +26,7 @@
                         For any inquiries, contact <a href="tel:02039822911 " class=" "> 02039822911</a>
                     </div>
                 </div> -->
+                <img src="https://travel24taxi.com/assets/images/travel24/Logo.svg" alt="urlllllll">
                 <img src="cid:logo_cid" alt="tetstttt">
                 <img src="cid:{logo_cid}" alt="Travel24 Logo" style="max-width: 100%;">
                 <p>message ---- <?php echo $mesg;?>,</p>
