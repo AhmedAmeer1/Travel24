54b0566a8ef0306a0ea46b582ac8892a8ed83741
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-04-23 20:26:36 +0530

    testing image

--- application/views/template/email.php
+++ application/views/template/email.php
@@ -16,15 +16,19 @@
         </div>
         <div style="padding: 20px; ">
             <div>
-                <!-- <div class="row ">
+               <div class="row ">
                     <div class="col-md-6">
                         <a class="mt-2" href="<?php echo base_url()?>"><img
-                                src="https://travel24taxi.com/assets/images/travel24/Logo.svg" alt=""></a>
+                                src="https://travel24taxi.com/assets/images/travel24/fleet/EstateCar.png" alt=""></a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="mt-2" href="<?php echo base_url()?>"><img
+                                src="https://via.placeholder.com/300x200.jpg" alt=""></a>
                     </div>
                     <div class="col-md-6 text-right">
                         For any inquiries, contact <a href="tel:02039822911 " class=" "> 02039822911</a>
                     </div>
-                </div> -->
+                </div>
 
 
                 <p>Hi <?php echo $first_name.' '.$last_name;?>,</p>
