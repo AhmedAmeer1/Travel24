1db7241fc5f34f69bc3aeb8918d87942e14a93db
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-05-19 14:56:42 +0530

    seo code added

--- application/views/common_components/contactusFooter.php
+++ application/views/common_components/contactusFooter.php
@@ -48,21 +48,21 @@
             </div>
         </div>
         <div class="row mt-md-4 mt-3">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class=" text-center text-md-left">
                     <span class="footer-p2 mt-3">
                         © Travel24 2017
                     </span>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class=" text-center ">
                     <span class="footer-p2 mt-3">
                     Travel 24 is the Trading of NoLimit Airport Cars Ltd
                     </span>
                 </div>
             </div>
-            <div class="col-md-4 ">
+            <div class="col-md-3 ">
                 <div class="text-center text-md-right">
                     <span class="footer-p2 mt-3">
                         <a href="<?php echo base_url()?>terms" class="terms">Terms & Conditions </a>
---
+++ assets/js/seo.js
@@ -0,0 +1,44 @@
+// seo.js
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "TaxiService",
+  "@graph": {
+    "@type": "SiteNavigationElement",
+    "@id": "https://travel24taxi.com#main",
+    "name": "Home",
+    "url": "https://travel24taxi.com/"
+  },
+  "provider": {
+    "@type": "LocalBusiness",
+    "name": "Travel 24 Taxi",
+    "description": "For reliable and professional airport transfers, Travel24 has you covered. We offer 24/7 minicab services to all UK airports for individuals and groups, with clear, upfront pricing.",
+    "telephone": "02039822911",
+    "image": {
+      "@type": "ImageObject",
+      "url": "https://travel24taxi.com/assets/images/travel24/Logo.svg",
+      "width": 279,
+      "height": 48
+    },
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Maidenhead",
+      "addressRegion": "Berkshire",
+      "postalCode": "SL6 4FJ",
+      "streetAddress": "Regus Maidenhead, Concorde Park. Concorde Road"
+    },
+    "location": {
+      "@type": "Place",
+      "geo": {
+        "@type": "GeoCircle",
+        "geoMidpoint": {
+          "@type": "GeoCoordinates",
+          "latitude": "53.5500",
+          "longitude": "2.4333"
+        },
+        "geoRadius": "50"
+      }
+    }
+  }
+}
+</script>
