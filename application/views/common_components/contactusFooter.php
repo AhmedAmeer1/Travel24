5b59c5aa870dd2cf0964fc3efd0e4af9ed8c14a7
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2024-08-17 15:09:03 +0530

    contact us footer updated

--- application/views/common_components/contactusFooter.php
+++ application/views/common_components/contactusFooter.php
@@ -1,15 +1,16 @@
 <link href="<?php echo base_url('assets/css/bootstrap.min1.css')?>" rel="stylesheet" />
 <link href="<?php echo base_url('assets/css/custom.css')?>" rel="stylesheet" />
-<link href="<?php echo base_url('assets/css/footer.css')?>" rel="stylesheet" />
+<link href="<?php echo base_url('assets/css/footer.css?v=8')?>" rel="stylesheet" />
 
 <footer class="limits-footer-wrapper">
     <div class="footer-container">
         <div class="mx-auto no-gutter text-center order-md-1 order-0">
-            <img src="<?php echo base_url('assets/images/travel24/Logo.svg')?>" class="img-fluid footr-logo" alt="Logo">
+            <a href="<?php echo base_url()?>"> <img src="<?php echo base_url('assets/images/travel24/Logo.svg')?>"
+                    class="img-fluid footr-logo" alt="Logo"></a>
 
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <h3 class="footer-h3">All major credit cards accepted</h3>
+
                     <ul class="social-media">
                         <li>
                             <a href="https://twitter.com/travel24taxi" target="_blank"><img
@@ -29,46 +30,46 @@
         </div>
 
         <div class="row mt-md-4 mt-3">
-            <div class="col-md-6">
+            <div class="col-md-12 text-center">
                 <ul class="side-menu ">
                     <!-- <li><a href="<?php echo base_url()?>blogs" class="footer-p1">Blogs</a></li> -->
                     <!-- <li><a href="<?php echo base_url()?>customerReviews" class="footer-p1">Reviews</a></li> -->
+                    <li><a href="<?php echo base_url()?>" class="footer-p1">Home</a></li>
                     <li><a href="<?php echo base_url()?>drivers" class="footer-p1">Drivers</a></li>
                     <li><a href="<?php echo base_url()?>aboutus" class="footer-p1">About Us</a></li>
+                    <li><a href="<?php echo base_url()?>popularDestinations" class="footer-p1">Destination</a></li>
+                    <li><a href="<?php echo base_url()?>faq" class="footer-p1">FAQ</a></li>
+                    <li><a href="<?php echo base_url()?>terms" class="footer-p1">Terms & Conditions</a></li>
                     <li><a href="<?php echo base_url()?>contactUs" class="footer-p1">Contact</a></li>
+
+
+
                 </ul>
             </div>
-            <div class="col-md-6 ">
-                <div class="text-right">
-                    <ul class="side-menu  ">
-                        <li>Tel :<a href="tel:02039822911" class="">02039822911 </a></li>
-                        <li>Email Us :<a href="mailto:info@travel24taxi.com" class="">info@travel24taxi.com</a></li>
-                    </ul>
-                </div>
+            <div class="col-md-12 text-center mt-3 ">
+
+                <ul class="side-menu  ">
+                    <span class="circle d-none d-md-inline-block"></span>
+                    <li class="contact">Tel :<a href="tel:02039822911" class="">02039822911 </a></li>
+                    <li class="contact">Email Us :<a href="mailto:info@travel24taxi.com"
+                            class="">info@travel24taxi.com</a></li>
+                </ul>
+
             </div>
         </div>
-        <div class="row mt-md-4 mt-3">
-            <div class="col-md-3">
-                <div class=" text-center text-md-left">
-                    <span class="footer-p2 mt-3">
-                        © Travel24 2017
-                    </span>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class=" text-center ">
-                    <span class="footer-p2 mt-3">
-                    Travel 24 is the Trading of NoLimit Airport Cars Ltd
-                    </span>
-                </div>
-            </div>
-            <div class="col-md-3 ">
-                <div class="text-center text-md-right">
-                    <span class="footer-p2 mt-3">
-                        <a href="<?php echo base_url()?>terms" class="terms">Terms & Conditions </a>
-                    </span>
-                </div>
-            </div>
+        <div class=" mt-md-4 mt-3   text-center">
+
+            <p class="footer-p2">
+                Travel 24 is the Trading of NoLimit Airport Cars Ltd
+            </p>
+            <span class="footer-p2 mt-3">
+                © Travel24 2017 - 2024
+            </span>
+
+
+
+
+
         </div>
 </footer>
 
