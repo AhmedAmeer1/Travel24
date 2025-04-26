6de52523c2d7b7e1a877b8a632789de212b7df3d
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2025-04-26 09:59:20 +0530

    testing is done

--- application/views/index.php
+++ application/views/index.php
@@ -164,7 +164,7 @@
 
                     <div class="col-md-6 banner-details">
                         <div class="mx-auto">
-                            <h1>Your Destination 111111 is our goal</h1>
+                            <h1>Your Destination is our goal</h1>
                             <p>
                                 Airport transfers & chauffeur services connecting all UK airports
                             </p>
