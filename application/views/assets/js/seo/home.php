bf9a58cb86d93938eabb15f865042ac77ea0c681
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2025-06-18 19:23:39 +0530

    site map updated

---
+++ application/views/assets/js/seo/home.php
@@ -0,0 +1,167 @@
+<script type="application/ld+json">
+{
+    "@context": "http://schema.org",
+    "@type": "TaxiService",
+    "name": "Travel 24 Taxi",
+    "image": "https://www.travel24taxi.com/assets/images/logo.png",
+    "@id": "https://www.travel24taxi.com/",
+    "url": "https://www.travel24taxi.com/",
+    "telephone": "+44 20 3982 2911",
+    "priceRange": "£30-£150",
+    "areaServed": {
+        "@type": "Place",
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "United Kingdom"
+        }
+    },
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Regus Maidenhead, Concorde Park, Concorde Road",
+        "addressLocality": "Maidenhead",
+        "addressRegion": "Berkshire",
+        "postalCode": "SL6 4FJ",
+        "addressCountry": "GB"
+    },
+    "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": 51.5164,
+        "longitude": -0.7229
+    },
+    "hasMap": "https://maps.app.goo.gl/G7amm9kYZrrocxu58",
+    "openingHoursSpecification": [{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+        ],
+        "opens": "00:00",
+        "closes": "23:59"
+    }],
+    "hasOfferCatalog": {
+        "@type": "OfferCatalog",
+        "name": "Travel 24 Taxi Services",
+        "itemListElement": [{
+                "@type": "OfferCatalog",
+                "name": "Brighton Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Brighton",
+                    "url": "https://travel24taxi.com/popularDestinations/view/brighton-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Bristol Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Bristol",
+                    "url": "https://travel24taxi.com/popularDestinations/view/bristol-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Gatwick Airport Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Gatwick Airport",
+                    "url": "https://travel24taxi.com/popularDestinations/view/gatwick-airport-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Guildford Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Guildford",
+                    "url": "https://travel24taxi.com/popularDestinations/view/guildford-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Heathrow Airport Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Heathrow Airport",
+                    "url": "https://travel24taxi.com/popularDestinations/view/heathrow-airport-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Luton Airport Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Luton Airport",
+                    "url": "https://travel24taxi.com/popularDestinations/view/luton-airport-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "London Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from London",
+                    "url": "https://travel24taxi.com/popularDestinations/view/london-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "London City Airport Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from London City Airport",
+                    "url": "https://travel24taxi.com/popularDestinations/view/London-city-airport-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Maidenhead Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Maidenhead",
+                    "url": "https://travel24taxi.com/popularDestinations/view/maidenhead-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Oxford Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Oxford",
+                    "url": "https://travel24taxi.com/popularDestinations/view/oxford-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Southampton Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Southampton",
+                    "url": "https://travel24taxi.com/popularDestinations/view/southampton-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Stansted Airport Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Stansted Airport",
+                    "url": "https://travel24taxi.com/popularDestinations/view/stansted-airport-transfer"
+                }
+            },
+            {
+                "@type": "OfferCatalog",
+                "name": "Windsor Taxi Transfer",
+                "itemOffered": {
+                    "@type": "Service",
+                    "name": "Taxi transfer to/from Windsor",
+                    "url": "https://travel24taxi.com/popularDestinations/view/windsor-transfer"
+                }
+            }
+        ]
+    },
+    "sameAs": [
+        "https://www.facebook.com/Travel24Taxi",
+        "https://www.instagram.com/Travel24Taxi"
+    ]
+}
+</script>
