b4a2ffc262eed07a9ed89aef41a878b0907d0c1b
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2025-09-07 22:00:31 +0530

    minor issue

--- application/views/listView.php
+++ application/views/listView.php
@@ -200,8 +200,8 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="details-box">
-                             <button type="button" id="success_modal" class="btn btn-info btn-lg" style="display:none"
-                                data-toggle="modal" data-target="#myModal">Open Modal</button>
+                            <!-- <button type="button" id="success_modal" class="btn btn-info btn-lg" style="display:none"
+                                data-toggle="modal" data-target="#myModal">Open Modal</button> -->
                             <div class="row">
                                 <div class="col-md-12 no-gutter">
                                     <div class="trip-d">
@@ -638,7 +638,7 @@
         if (timeDifferenceMilliseconds < threeHoursInMilliseconds) {
             alert(
                 "The entered date and time is less than 3 hours. Please book at least 3 hours in advance or call us."
-                );
+            );
             return;
         }
 
@@ -717,21 +717,18 @@
 
                 if (payment_method === "cash") {
                     // 👉 As requested: redirect to thank-you page (no modal)
+                    // var obj = jQuery.parseJSON(data);
 
+                    // if (obj.result['booking_id'] != "") {
+                    //     $("#book_id").text(obj.result['booking_id']);
+                    //     get_book_details(obj.result['booking_id'])
+                    //     $("#success_modal").trigger('click')
 
-                 var obj = jQuery.parseJSON(data);
-
-                        if (obj.result['booking_id'] != "") {
-                            $("#book_id").text(obj.result['booking_id']);
-                            get_book_details(obj.result['booking_id'])
-                            $("#success_modal").trigger('click')
-
-                        }
-
-                    // alert(
-                    //     "Thank you for your booking! We have sent you a email for the confirmation ."
-                    //     );
-                    // window.location.replace('<?php echo base_url('/')?>');
+                    // }
+                    alert(
+                        "Thank you for your booking! We have sent you a email for the confirmation ."
+                    );
+                    window.location.replace('<?php echo base_url('/')?>');
                 } else if (payment_method === "lloyds") {
                     // keep original IPG behaviour (bank page in new tab/window)
                     window.open('<?php echo base_url('index/ipg')?>');
