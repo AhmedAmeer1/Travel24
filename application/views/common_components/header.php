c03282183ec71ddd4a66ba4f0c033cff24c78fd8
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2025-10-20 22:34:49 +0530

    test 22

--- application/views/common_components/header.php
+++ application/views/common_components/header.php
@@ -27,9 +27,9 @@
                             <a class="nav-link" href="<?php echo base_url()?>customerReviews">reviews</a>
                         </li>
 
-                         <li class="nav-item">
+                         <!-- <li class="nav-item">
                             <a class="nav-link" href="<?php echo base_url()?>bookNow">BookNow</a>
-                        </li>
+                        </li> -->
                         <li class="nav-item mt-1">
                             <a href="tel:02039822911 " class=" call-div">
                                 <img src="<?php echo base_url('assets/images/travel24/call.svg')?>" alt="Call Icon"> 02039822911</a>
