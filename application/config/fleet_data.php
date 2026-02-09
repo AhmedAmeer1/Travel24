be09a524e2d79e931705e55401379d226725e139
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2026-02-09 08:38:48 +0530

    order changed

--- application/config/fleet_data.php
+++ application/config/fleet_data.php
@@ -24,28 +24,10 @@ $config['fleet_data'] = [
         'vehicle_image' => 'PeopleCarrier.png'
     ],
 
-    [
-        'title' => 'EXECUTIVE CAR',
-        'description' => 'This vehicle can accommodate up to 2 passengers with 2 suitcases and 2 hand luggage or up to 3 passengers for the trip.',
-        'noOfPassengers' => '2',
-        'noOfSuitcases' => '2',
-         'vehicle_image' => 'ExecutiveCar.png'
-    ],
-    [
-        'title' => '8 SEATER MINIBUS (Van)',
-        'description' => 'This vehicle can accommodate up to 7 passengers with 7 suitcases and  7 hand luggage or up to 8 passengers for the trip.',
-        'noOfPassengers' => '7',
-        'noOfSuitcases' => '7',
-        'vehicle_image' => '8SeaterMiniBus.png'
-    ],
-    [
-        'title' => 'EXECUTIVE PEOPLE CARRIER',
-        'description' => 'This vehicle can accommodate up to 6 passengers with 6 suitcases and 6 hand luggage or up to 7 passengers for the trip.',
-        'noOfPassengers' => '6',
-        'noOfSuitcases' => '6',
-        'vehicle_image' => 'ExecutivePeopleCarrier.png'
-    ],
-    [
+
+
+
+  [
         'title' => 'MOBILITY VEHICLE',
         'description' => 'This vehicle can accommodate up to 5 passengers with 5 suitcases and 5 hand luggage or up to 7 passengers for the trip.',
         'noOfPassengers' => '5',
@@ -53,7 +35,30 @@ $config['fleet_data'] = [
         'vehicle_image' => 'MobilityVehicle.png'
     ],
 
+ [
+        'title' => 'EXECUTIVE PEOPLE CARRIER',
+        'description' => 'This vehicle can accommodate up to 6 passengers with 6 suitcases and 6 hand luggage or up to 7 passengers for the trip.',
+        'noOfPassengers' => '6',
+        'noOfSuitcases' => '6',
+        'vehicle_image' => 'ExecutivePeopleCarrier.png'
+    ],
 
+
+    [
+        'title' => '8 SEATER MINIBUS (Van)',
+        'description' => 'This vehicle can accommodate up to 7 passengers with 7 suitcases and  7 hand luggage or up to 8 passengers for the trip.',
+        'noOfPassengers' => '7',
+        'noOfSuitcases' => '7',
+        'vehicle_image' => '8SeaterMiniBus.png'
+    ],
+   
+      [
+        'title' => 'EXECUTIVE CAR',
+        'description' => 'This vehicle can accommodate up to 2 passengers with 2 suitcases and 2 hand luggage or up to 3 passengers for the trip.',
+        'noOfPassengers' => '2',
+        'noOfSuitcases' => '2',
+         'vehicle_image' => 'ExecutiveCar.png'
+    ],
    
     // Add the rest of your destinations here...
 ];
